ef07d82cd4268e2b725076e097b9a6e3e38a5bca
cannondalev2000 <cannondalev2000@gmail.com>
2014-05-31 21:46:46 -0700

    change to cortex a15 instead of default a9
    anything useing this tree is a15 not a9
    
    Change-Id: Icb8711a76aac63707b37dd761f02db1d1a206c4c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,6 +39,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a15
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
